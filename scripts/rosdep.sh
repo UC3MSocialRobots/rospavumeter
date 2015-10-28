11c9619a0bfe7b7b6b75008b8310ee51493b6bcd
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2015-10-28 19:56:56 +0100

    missing dep

--- scripts/rosdep.sh
+++ scripts/rosdep.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # External, ROS and system package dependencies
 
-sudo apt-get install libglibmm-2.4 libglib2.0 libsigc++
-
-exit 0
+sudo apt-get install libglibmm-2.4 libglib2.0 libsigc++ libpulse-dev
